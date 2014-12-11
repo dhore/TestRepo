645139aa0818118afb2f3087b804eaeaf0e27437
Magma Lair <Magmalair@hotmail.com>
2014-12-11 19:54:27 +1100

    - Pacman movement simplified and updated

---
+++ PaleBlueProject/Assets/Standard Assets/Character Controllers/Sources/Scripts/ContinueForward.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ContinueForward : MonoBehaviour {
+
+	public bool forwardStop = true;
+	float forwardDelay = 3.0f;
+	float speed = 10.0f;
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (forwardStop == false)
+			forwardDelay -= Time.deltaTime;
+
+		if (forwardDelay <= 0.0f)
+			transform.Translate(0, 0, speed*Time.deltaTime);
+	}
+}
