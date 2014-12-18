f407aa986a7c1a4f41029030e1484ee6ea08e849
Magma Lair <Magmalair@hotmail.com>
2014-12-18 18:54:42 +1100

    + PacDots Functional

---
+++ PaleBlueProject/Assets/PacDots/PacDot_Float.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PacDot_Float : MonoBehaviour {
+
+	float speed = 0.5f;
+	float timer = 0.75f;
+	bool down = true;
+
+
+	// Update is called once per frame
+	void Update ()
+	{
+		timer -= Time.deltaTime;
+		if (timer <= 0)
+		{
+			down = !down;
+			timer = 0.75f;
+		}
+	
+		if (down == true)
+			transform.Translate(0, -speed*Time.deltaTime, 0);
+		else
+			transform.Translate(0, speed*Time.deltaTime, 0);
+	}
+
+	void OnCollisionEnter (Collision coll)
+	{
+		if (coll.transform.tag == "Player")
+			Destroy (this.gameObject);
+	}
+}
